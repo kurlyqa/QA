8623e8a677efb87299b503a556966f757e06da45
Jun-Kyung <asd8680@naver.com>
2023-04-13 17:18:32 +0900

    LMS_Login sample script

---
+++ Kurly/Web/LMS_Login.py
@@ -0,0 +1,153 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+import subprocess
+
+class LMS_Login(unittest.TestCase):
+    def setUp(self):
+        # 사람처럼 보이게 하는 옵션들
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('disable-gpu')  # 가속(GPU) 사용 안함
+        chrome_options.add_argument('lang=ko_KR')  # 언어 설정
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)  # 크롬 드라이버 설치
+        # 웹페이지 전체가 로드 될때까지 기다린다.
+        self.driver.implicitly_wait(30) # 또는 self.driver.set_page_load_timeout(30)
+
+    # 반복 되는 부분을 모듈화 한 함수
+    def interact(self, by_type, name, wait_sec=1, click=True, send_keys_msg=None):
+        if by_type == 'XPATH':
+            if send_keys_msg == None:
+                if click == True:
+                    ele = self.driver.find_element(by=By.XPATH, value=name)
+                    self.driver.execute_script("arguments[0].setAttribute('style',arguments[1]);", ele, "background: yellow; border: 2px solid red;")
+                    self.driver.execute_script("arguments[0].click();", ele)
+                elif click == False:
+                    ele = self.driver.find_element(by=By.XPATH, value=name)
+                    self.driver.execute_script("arguments[0].setAttribute('style',arguments[1]);", ele, "background: yellow; border: 2px solid red;")
+            else:
+                self.driver.find_element(by=By.XPATH, value=name).send_keys(send_keys_msg)
+            sleep(wait_sec)
+        elif by_type == 'NAME':
+            if send_keys_msg == None:
+                if click == True:
+                    ele = self.driver.find_element(by=By.NAME, value=name)
+                    self.driver.execute_script("arguments[0].setAttribute('style',arguments[1]);", ele, "background: yellow; border: 2px solid red;")
+                    self.driver.execute_script("arguments[0].click();", ele)
+                elif click == False:
+                    ele = self.driver.find_element(by=By.NAME, value=name)
+                    self.driver.execute_script("arguments[0].setAttribute('style',arguments[1]);", ele, "background: yellow; border: 2px solid red;")
+            else:
+                self.driver.find_element(by=By.NAME, value=name).send_keys(send_keys_msg)
+            sleep(wait_sec)
+
+    def test_01_Login(self):
+        # 이동할 url주소
+        url = 'https://lms.stg.kurly.com/#/login'
+        # url 이동
+        self.driver.get(url)
+        # 브라우저 최대화
+        self.driver.maximize_window()
+
+        ## LMS 모바일 로그인
+        # 아이디 입력
+        self.interact(by_type="XPATH", name="//input[@id='input-13' and @type='text' and @required='required']", click=False, send_keys_msg='junhyunkyung')
+        # 비밀번호 입력
+        self.interact(by_type="XPATH", name="//input[@id='input-16' and @type='password' and @required='required']", click=False, send_keys_msg='!tlgjatlf1')
+        # 로그인
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn__content')]")
+
+        # 출근 클릭
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'work-btn v-btn v-btn--contained v-btn--fab v-btn--round theme--light v-size--default primary')]")
+        # 송파CC
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[1]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), '송파')]")
+        # 송파 냉장1
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[2]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), '송파 냉장1')]")
+        # 다음
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn v-btn--contained theme--light v-size--default primary')]")
+        # 업무파트=IB
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[1]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), 'IB')]")
+        # 팀명=ABC
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[2]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), 'ABC')]")
+        # 근무 Shift=00:40 ~ 01:30
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[3]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), '00:40 ~ 01:30')]")
+        # 다음
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn v-btn--contained theme--light v-size--default primary')]")
+        # 아니요
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'bottom-btn no-btn v-btn v-btn--contained theme--light v-size--default')]")
+        # 네
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'dialog-btn ml-0 v-btn v-btn--contained theme--light v-size--default primary')]")
+
+        ## LMS 어드민 로그인
+        # 새 탭 열기
+        self.driver.execute_script("window.open('');")
+        # 새로 열린 탭으로 포커스 변경
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        # LMS 어드민 URL 접속
+        self.driver.get("https://admin-lms.stg.kurly.com/?#/login")
+        # 아이디 입력
+        self.interact(by_type="XPATH", name="//input[@id='input-16' and @type='text' and @required='required']", click=False, send_keys_msg='junhyun.kyung')
+        # 비밀번호 입력
+        self.interact(by_type="XPATH", name="//input[@id='input-19' and @type='password' and @required='required']", click=False, send_keys_msg='!tlgjatlf1')
+        # 로그인
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn v-btn--block v-btn--contained theme--light v-size--large primary')]")
+        # 확인
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn v-btn--block v-btn--flat v-btn--text theme--light v-size--default primary--text')]")
+        # 현장관리 탭
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn__content') and contains(text(), '현장관리')]")
+        # CC = 송파CC
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[1]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), '송파 CC')]")
+        # 센터 = 송파 냉장1 센터
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[2]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), '송파 냉장1')]")
+        # 업무파트 = IB
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[3]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), 'IB')]")
+        # 대분류 작업공정 = picking
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[4]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), 'picking')]")
+        # 소분류 작업공정 = 111
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-select__selections')])[5]")
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-list-item__title') and contains(text(), '111')]")
+        # QR코드 junhyunkyung 입력
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-input__slot white')])[6]//input", click=False, send_keys_msg='junhyunkyung')
+        # 체크인
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn__content') and contains(text(), '체크인/체크아웃')]")
+        # QR코드 junhyunkyung 입력
+        self.interact(by_type="XPATH", name="(//*[contains(@class, 'v-input__slot white')])[6]//input", click=False, send_keys_msg='junhyunkyung')
+        # 체크 아웃
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'v-btn__content') and contains(text(), '체크인/체크아웃')]")
+        # LMS 어드민 탭 닫기
+        self.driver.close()
+        # 포커스를 LMS 모바일로 변경
+        self.driver.switch_to.window(self.driver.window_handles[0])
+
+        # 퇴근
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'work-btn v-btn v-btn--contained v-btn--fab v-btn--round theme--light v-size--default primary')]")
+        # 네
+        self.interact(by_type="XPATH", name="//*[contains(@class, 'dialog-btn ml-0 v-btn v-btn--contained theme--light v-size--default primary')]")
+        # LMS 모바일 닫기
+        self.driver.close()
+        print('테스트 정상 종료!')
+
+        # 계속 생성되는 크롬 아이콘 없애기 위해 크롬창 모두 닫기(기존에 사용하던 chrome창은 살리고 싶으면 주석처리 해도 됨)
+        # subprocess.run(['pkill', '-f', 'Google Chrome'])
+
+    def tearDown(self):
+        self.driver.quit()
+
+## 이 클래스에서 정의된 테스트 메소드를 찾아서 실행하고, 그 결과를 출력하는 코드
+# Python에서 모듈이 직접 실행될 때 (즉, 다른 모듈에서 import 되지 않고 직접 실행될 때) 해당 코드 블록을 실행하도록 하는 일종의 조건문
+if __name__ == '__main__':
+    # 이 클래스에서 정의된 테스트 메소드들을 자동으로 찾아주는 메소드를 사용하여 테스트 스위트(TestSuite) 객체를 생성
+    suite = unittest.TestLoader().loadTestsFromTestCase(LMS_Login)
+    # TextTestRunner 클래스의 객체를 생성하여 생성된 테스트 스위트 객체를 실행. verbosity 인자는 테스트 결과를 출력할 상세도를 설정하는 인자. 2이면 테스트 케이스 수, 테스트 시간, 테스트 결과를 출력. .run(suite)는 생성된 TestSuite객체를 실행하는 메소드.
+    unittest.TextTestRunner(verbosity=2).run(suite)
--- Kurly/Web/TestGroup.py
+++ Kurly/Web/TestGroup.py
@@ -3,8 +3,7 @@
 from  __future__ import print_function
 from unittest import TestLoader, TestSuite
 from HtmlTestRunner import HTMLTestRunner
-from Login import *
-from Category import *
+from LMS_Login import *
 from email.message import EmailMessage
 import datetime
 import os
@@ -18,17 +17,16 @@ import smtplib
 #
 #     dname = json_string['dname']
 
-s1 = TestLoader().loadTestsFromTestCase(Login)
-s2 = TestLoader().loadTestsFromTestCase(Category)
+s1 = TestLoader().loadTestsFromTestCase(LMS_Login)
 
-suite = TestSuite([s1, s2])
+suite = TestSuite([s1])
 
 daytime = datetime.datetime.now()
 dt = daytime.strftime("%Y-%m-%d_%H-%M-%S")
-filename = (f"GalaxyNote9MoaAutomationFunctionalTestReport")
+filename = (f"GalaxyS22PlusKurlyAutomationFunctionalTestReport")
 dir = os.getcwd()
-finalfile = (dir + f"/reports/GalaxyNote9MoaAutomationTestReport_"+dt+".html")
-runner = HTMLTestRunner(combine_reports=True, report_name=filename, report_title="Moa Automation Functional Test Report (Android)") # report_title 파일열면 가장위에 있는 메인 title
+finalfile = (dir + f"/reports/GalaxyS22PlusKurlyAutomationTestReport_"+dt+".html")
+runner = HTMLTestRunner(combine_reports=True, report_name=filename, report_title="Kurly Automation Functional Test Report (Android)") # report_title 파일열면 가장위에 있는 메인 title
 runner.run(suite)
 
 # smtp_gmail = smtplib.SMTP('smtp.cafe24.com', 587) # 발신 메일서버 포트번호
@@ -36,13 +34,13 @@ runner.run(suite)
 # smtp_gmail.ehlo() # SMTP 객체를 생성한 후에는 프로토콜 상 가장 먼저 SMTP 서버에 Hello 메시지를 보내는데, ehlo() 메서드가 이 기능을 함
 # smtp_gmail.starttls() # tls방식으로 접속, 그 포트번호가 587
 # smtp_gmail.login('billy@buzzni.com', '!tlgjatlf1')
-# msg['Subject'] = "Moa Automation Test Report (Android)" # 메일제목
+# msg['Subject'] = "Kurly Automation Test Report (Android)" # 메일제목
 # msg.set_content("Test Started at : %s // Test Completed at : %s"%(daytime, datetime.datetime.now())) # 메일내용 테스트 시작시간과 종료시간
-# msg['From'] = 'billy@buzzni.com'
-# msg['To'] = 'billy@buzzni.com'
+# msg['From'] = 'junhyun.kyung@kurlycorp.com'
+# msg['To'] = 'junhyun.kyung@kurlycorp.com'
 # # msg['Cc'] = ['a@b.com','a@b.com','a@b.com'] # 참조
 # file = finalfile
 # fp = open(file, 'rb')
 # file_data = fp.read()
-# msg.add_attachment(file_data, maintype='text', subtype='plain', filename="MoaAutomationTestReport_"+dt+".html")
+# msg.add_attachment(file_data, maintype='text', subtype='plain', filename="KurlyAutomationTestReport_"+dt+".html")
 # smtp_gmail.send_message(msg)
