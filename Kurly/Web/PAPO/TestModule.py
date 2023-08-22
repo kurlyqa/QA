de58c24a46eb81395ecb32676b52b621a446151d
Jun-Kyung <asd8680@naver.com>
2023-08-22 09:25:51 +0900

    TestModule update

--- Kurly/Web/PAPO/TestModule.py
+++ Kurly/Web/PAPO/TestModule.py
@@ -16,13 +16,8 @@ class testModule(unittest.TestCase):
         # headless 모드 설정
         chrome_options.add_argument('headless')
 
-        # 크롬 위치 명시
-        chrome_options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-        chrome_driver_binary = "/usr/local/bin/chromedriver"
-
-        # 크롬 드라이버 설치
-        chrome_driver_service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(executable_path=chrome_driver_binary, options=chrome_options, service=chrome_driver_service)
+        # 크롬 드라이버를 생성
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
         # 웹페이지 전체가 로드 될때까지 기다림
         self.driver.implicitly_wait(30) # 또는 self.driver.set_page_load_timeout(30)
